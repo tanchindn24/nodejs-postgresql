2a13d45d62208b962612e338516384b2d5e0979f
tanchindn24 <phamtruong24042001@gmail.com>
2023-11-25 00:50:42 +0700

    deploy 3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 })
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const router = require('./src/student/routes');
 
 app.use(express.json());
--- src/student/controller.js
+++ src/student/controller.js
@@ -8,7 +8,7 @@ const queries = require('../student/queries');
 const axios = require("axios");
 
 const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const migrate = async (req, res) => {
     console.log('migrate');
